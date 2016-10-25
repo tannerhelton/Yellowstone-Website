d73e636b5ea4255ca8dd4052a6f979b378288ff5
tannerwh <tannerwh@gmail.com>
2016-10-25 18:08:10 -0500

    Added modal images

--- server.js
+++ server.js
@@ -9,38 +9,4 @@ var server = http.listen(3000, function() {
 	console.log('Server listening on http://localhost:3000/');
 });
 
-var users = [];
-
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function(socket) {
-	socket.emit('welcome', { text : 'You have been connected!' });
-
-	socket.on('user', function (name) {
-		console.log(name + ' connected');
-		users.push(name);
-		socket.user = name;
-		console.log('Current users : ' + users.length);
-		socket.broadcast.emit('otherUserConnect', name);
-	});
-	
-	socket.on('disconnect', function() {
-		if(!socket.user) {
-			return;
-		}
-		if(users.indexOf(socket.user) > -1) {
-			console.log(socket.user + ' disconnected');
-			users.splice(users.indexOf(socket.user), 1);
-			socket.broadcast.emit('otherUserDisconnect', socket.user);
-		}
-	});
-	
-	socket.on('message', function(data) {
-		io.sockets.emit('message', {
-			user: socket.user,
-			message: data
-		});
-	});
-});
-
 
